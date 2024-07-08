c3b948182047d7597c81eece544fab9b25663e93
Oyindamola .P. Dosumu <introvertllc2019@gmail.com>
2024-07-08 16:38:09 +0100

    lol

--- aui-cooperatives/src/App.tsx
+++ aui-cooperatives/src/App.tsx
@@ -9,25 +9,38 @@ import Security from "./pages/Security";
 import IndividualLoan from "./pages/ILoan";
 import AnnouncementsBar from "./component/AnnouncementsBar";
 import Tabs from "./component/Tabs";
+import { useState } from "react";
 
 function App() {
+  const user = null;
   const userName = "Ukachi Charles";
   const userProfilePicture = "./src/images/dummy profile.jpeg";
 
   return (
-    <div className=" flex flex-row justify-between h-screen min-w-screen">
-      <Sidebar userName={userName} userProfilePicture={userProfilePicture} />
-      <Routes>
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/savings" element={<Savings />} />
-        <Route path="/management" element={<Management />} />
-        <Route path="/loan" element={<IndividualLoan />} />
-        <Route path="/announcement" element={<Announcement />} />
-        <Route path="/security" element={<Security />} />
-        <Route path="/settings" element={<Settings />} />
-      </Routes>
-      <AnnouncementsBar />
-    </div>
+    <>
+      {user ? (
+        <div className=" flex flex-row justify-between h-screen min-w-screen">
+          <Sidebar
+            userName={userName}
+            userProfilePicture={userProfilePicture}
+          />
+          <Routes>
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/savings" element={<Savings />} />
+            <Route path="/management" element={<Management />} />
+            <Route path="/loan" element={<IndividualLoan />} />
+            <Route path="/announcement" element={<Announcement />} />
+            <Route path="/security" element={<Security />} />
+            <Route path="/settings" element={<Settings />} />
+          </Routes>
+          <AnnouncementsBar />
+        </div>
+      ) : (
+        <Routes>
+          <Route path="" element={<Tabs />} />
+        </Routes>
+      )}
+    </>
   );
 }
 
